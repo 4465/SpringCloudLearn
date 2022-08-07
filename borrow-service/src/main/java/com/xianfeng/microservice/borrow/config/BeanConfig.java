5847481fef9adb015b8f73b8b7013e0e260c206f
4465 <lwfrekach@gmail.com>
2022-08-07 23:02:35 +0800

    负载均衡

--- borrow-service/src/main/java/com/xianfeng/microservice/borrow/config/BeanConfig.java
+++ borrow-service/src/main/java/com/xianfeng/microservice/borrow/config/BeanConfig.java
@@ -2,11 +2,14 @@ package com.xianfeng.microservice.borrow.config;
 
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@LoadBalancerClient(value = "userservice",
+                    configuration = LoadBalancerConfig.class)
 public class BeanConfig {
     @Bean
     @LoadBalanced
